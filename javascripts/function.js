e432be58319cac40b8e3422bc3de584f936cf11b
fabiosachetto <fabio@sachetto.com.br>
2014-02-06 00:20:32 -0200

    add function com o parametros do plugin de contato

--- javascripts/function.js
+++ javascripts/function.js
@@ -25,6 +25,20 @@ $(".various").fancybox({
     closeEffect : 'elastic'
 });
 
-$('#contactable').contactable({
-    url: 'mail.php'
-});
+jQuery(function(){
+        jQuery('#contactable').contactable(
+        {
+            subject: 'feedback URL:'+location.href,
+            url: 'mail.php',
+            name: 'Name',
+            email: 'Email',
+            dropdownTitle: 'Issue',
+            dropdownOptions: ['General', 'Website bug', 'Feature request'],
+            message : 'Message',
+            submit : 'SEND',
+            recievedMsg : 'Thank you for your message',
+            notRecievedMsg : 'Sorry but your message could not be sent, try again later',
+            disclaimer: 'Please feel free to get in touch, we value your feedback',
+            hideOnSubmit: true
+        });
+    });
